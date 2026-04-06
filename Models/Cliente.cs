cead0d5d23e6a6a1647359dc79fef945dfed3c65
Antônio Covas <antoniocovasjr@hotmail.com>
2026-04-06 19:18:06 -0300

    feat: adicionando novas validações no back pra modelo cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -18,14 +18,39 @@ namespace AspNetCoreWebApp.Models
         public DateTime DataNascimento { get; set; }
         
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-        [MaxLength(11, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
         [RegularExpression(@"[0-9]{11}$", ErrorMessage = "O campo {0} deve ser preenchido com 11 dígitos numéricos.")]
-        [UIHint("_CustomCPF")]
+        [MaxLength(11, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        [MinLength(11, ErrorMessage = "O campo {0} deve conter no mínimo {1} caracteres.")]
+        [UIHint("_CPFTemplate")]
         public string CPF { get; set; }
+
+        //[Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        //[RegularExpression(@"[0-9]{11}$", ErrorMessage = "O campo {0} deve ser preenchido com 11 dígitos numéricos.")]
+        //[MaxLength(11, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
+        //[MinLength(11, ErrorMessage = "O campo {0} deve conter no mínimo {1} caracteres.")]
+        //[UIHint("_TelefoneTemplate")]
+        //public string Telefone { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [DisplayName("E-mail")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve conter no máximo {1} caracteres.")]
         [EmailAddress(ErrorMessage = "O campo {0} deve conter um endereço de e-mail válido.")]
         public string? Email { get; set; }
+
+        //[Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        //[DisplayName("Situação")]
+        //public SituacaoCliente Situacao { get; set; }
+
+        //public Endereco Endereco { get;set; }
+
+        //public List<Pedido> Pedidos { get; set; }
+
+        public enum SituacaoCliente
+        {
+            Bloqueado,
+            Cadastrado,
+            Aprovado,
+            Especial
+        }
     }
 }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -6,16 +6,18 @@ namespace AspNetCoreWebApp.Models
     public class Produto
     {
         [Key]
+        [Display(Name = "Código")]
+        [DisplayFormat(DataFormatString = "{0:D60,}")]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-        [MaxLength(100)]
-        public string Nome { get; set; }
+        [MaxLength(100, ErrorMessage = "O campo \"{0}\" pode ter até {1} caracteres.")]
+        public string? Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-        [MaxLength(1000)]
+        [MaxLength(1000, ErrorMessage = "O campo \"{0}\" pode ter até {1} caracteres.")]
         [Display(Name = "Descrição")]
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
         
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [Column(TypeName = "decimal(18,2)")]
